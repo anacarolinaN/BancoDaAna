f81f892db4b6a234f5a5b155dd31721aae6307c6
Gabriel <gabrielsilvaplay16@gmail.com>
2017-08-24 16:11:26 -0300

    babozeira

---
+++ anabanco/src/main/java/cliente.java
@@ -0,0 +1,22 @@
+
+import java.util.Date;
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author aluno
+ */
+public class cliente {
+    
+    string nome;
+    int rg;
+    int cpf;
+    Date dt_nasc;
+    
+    
+}
---
+++ anabanco/src/main/java/conta.java
@@ -0,0 +1,20 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author aluno
+ */
+public class conta {
+    
+    int numero;
+    string agencia;
+    int saldo;
+    int limite;
+    
+    
+    
+}
